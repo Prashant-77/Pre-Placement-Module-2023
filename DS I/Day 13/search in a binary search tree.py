7f70cdc4716a1ad8289e64285e51caf82fec2696
Prashant Prakash <97874555+Prashant-77@users.noreply.github.com>
2022-07-22 23:35:42 +0530

    Create search in a binary search tree.py

---
+++ DS I/Day 13/search in a binary search tree.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        return self.searchBST(root.right, val)
